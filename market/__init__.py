701b9f452c241077a7ccb109373e71fb9f4e9ac1
Mohammad Ashfand <ashfand.s@simformsolutions.com>
2021-10-05 17:54:45 +0530

    register form

--- market/__init__.py
+++ market/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config['SECRET_KEY'] = 'bed0b3c89d25f30eae4de1b2'
 db = SQLAlchemy(app)
 
 from market import routes
