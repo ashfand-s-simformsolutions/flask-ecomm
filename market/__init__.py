0a43795a0b4bfce642b0ddbefa331ab362ae5761
Mohammad Ashfand <ashfand.s@simformsolutions.com>
2021-10-08 12:50:23 +0530

    buy sell functionality

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
-app.config['SECRET_KEY'] = 'bed0b3c89d25f30eae4de1b2'
+app.config["SECRET_KEY"] = "bed0b3c89d25f30eae4de1b2"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from market import routes
